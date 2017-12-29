17365422f8cb6200893b43007e4a590e8d5d3538
Arpan Patel <arpan6884@gmail.com>
2017-12-29 10:27:40 -0600

    added more javascript

--- Javascript/javascript/learn_to_code.js
+++ Javascript/javascript/learn_to_code.js
@@ -1,6 +1,15 @@
-var name = "Jack";
+//var name = "Jack";
+//var age = 23;
+//
+//var message = "Hi , my name is  " + name +  "  and I am  " + age + " years old";
+
+var firstName = "Lalu";
+var lastName = "Patel";
+var dateOfBirth = "10-09-1994";
 var age = 23;
 
-var message = "Hi , my name is  " + name +  "  and I am  " + age + " years old";
+var profileImgUrl = "http://coolpicks.com/lalu.jpg";
 
-console.log(message);
+
+var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age +  "rd birthday!";
+console.log(loginWelcomeMessage);
