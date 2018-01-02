88b8a75fb4e12691bdcaadc8861c28bdad154bdb
apatel183 <arpanpatel@dhcp-10-105-91-215.wireless.northwestern.private>
2018-01-02 09:23:36 -0600

    Javescript execrises

---
+++ Javascript/javascript/loops.js
@@ -0,0 +1,13 @@
+//var total = 10;
+//for (var x = 0; x < total; x++){
+//    console.log(x);
+//}
+
+
+var students = ["Lalu", "Jay", "Akshay", "Parth", "Keyur"];
+
+for (var x = 0; x < students.length; x++){
+    console.log(students[x]);  
+}
+
+//console.log(students[0]); 
